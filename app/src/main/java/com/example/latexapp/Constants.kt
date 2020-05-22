7d081ff552e9bfbe79c05fafa86d71c842e6def7
robinkamboj <robin.kamboj@instasolv.com>
2020-05-22 13:33:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/latexapp/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.latexapp
+
+class Constants {
+    companion object {
+        const val SAMPLE_LATEX = "F(x) &= \\int^a_b \\frac{1}{3}x^3\\\\\\frac{1}{\\sqrt{x}}"
+    }
+}
---
+++ app/src/main/java/com/example/latexapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.latexapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setup()
+    }
+
+    private fun setup() {
+        //todo - do something
+    }
+}
